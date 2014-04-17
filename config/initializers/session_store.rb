a7d383c4ee3546842bb95d9e7c65f51cd20af678
joker1007 <kakyoin.hierophant@gmail.com>
2014-04-18 01:49:58 +0900

    Update rails4.1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-TransactionTest::Application.config.session_store :cookie_store, key: '_transaction_test_session'
+Rails.application.config.session_store :cookie_store, key: '_transaction_test_session'
